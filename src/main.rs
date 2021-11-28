5a6a4f0d43147237150bf6938b356c5ac7c752dd
Luj8n <l.milieska@gmail.com>
2021-11-28 13:18:33 +0200

    Sudoku struct removed

--- src/main.rs
+++ src/main.rs
@@ -1,106 +1,72 @@
 use rayon::prelude::*;
 use std::time::Instant;
 
-#[derive(Clone, PartialEq)]
-struct Sudoku {
-  array: [u8; 81],
-}
+fn can_place(sudoku: &[u8; 81], x: usize, y: usize, digit: u8) -> bool {
+  assert!(x < 9 && y < 9 && digit < 10);
 
-impl Sudoku {
-  fn new(starting_array: [u8; 81]) -> Self {
-    Sudoku { array: starting_array }
-  }
+  for i in 0..9 {
+    let row_i = sudoku[y * 9 + i];
+    let collumn_i = sudoku[i * 9 + x];
+    let square_i = sudoku[(x / 3) * 3 + i % 3 + (y / 3) * 9 * 3 + (i / 3) * 9];
 
-  fn from_str(starting_string: &str) -> Self {
-    assert_eq!(starting_string.len(), 81);
-    let mut array: [u8; 81] = [0; 81];
-    for (i, char) in starting_string.chars().enumerate() {
-      let digit = char.to_digit(10).unwrap() as u8;
-      assert!(digit < 10);
-      array[i] = digit;
+    if row_i == digit || collumn_i == digit || square_i == digit {
+      return false;
     }
-    Self { array }
   }
 
-  fn get(&self, x: usize, y: usize) -> &u8 {
-    assert!(x < 9 && y < 9);
-    &self.array[y * 9 + x]
+  true
+}
+
+fn string_to_array(starting_string: &str) -> [u8; 81] {
+  assert_eq!(starting_string.len(), 81);
+  let mut array: [u8; 81] = [0; 81];
+  for (i, char) in starting_string.chars().enumerate() {
+    let digit = char.to_digit(10).unwrap() as u8;
+    assert!(digit < 10);
+    array[i] = digit;
   }
+  array
+}
 
-  fn change(&mut self, x: usize, y: usize, new_value: u8) {
-    assert!(x < 9 && y < 9 && new_value < 10);
-    self.array[y * 9 + x] = new_value;
-  }
-
-  fn can_place(&self, x: usize, y: usize, digit: u8) -> bool {
-    assert!(x < 9 && y < 9 && digit < 10);
-
-    for i in 0..9 {
-      let row_i = self.array[y * 9 + i];
-      let collumn_i = self.array[i * 9 + x];
-      let square_i = self.array[(x / 3) * 3 + i % 3 + (y / 3) * 9 * 3 + (i / 3) * 9];
-
-      if row_i == digit || collumn_i == digit || square_i == digit {
-        return false;
-      }
+fn array_to_string(array: [u8; 81]) -> String {
+  let mut string = String::new();
+  for y in 0..9 {
+    if y % 3 == 0 && y != 0 {
+      string += &format!("{}\n", &["-----"; 3].join("+"));
     }
-
-    true
-  }
-}
-
-impl Default for Sudoku {
-  fn default() -> Self {
-    Self { array: [0; 81] }
-  }
-}
-
-impl ToString for Sudoku {
-  fn to_string(&self) -> String {
-    let mut string = String::new();
-    for y in 0..9 {
-      if y % 3 == 0 && y != 0 {
-        string += &format!("{}\n", &["-----"; 3].join("+"));
+    for x in 0..9 {
+      if x % 3 == 0 && x != 0 {
+        string += "|";
       }
-      for x in 0..9 {
-        if x % 3 == 0 && x != 0 {
-          string += "|";
-        }
-        string += &self.array[y * 9 + x].to_string();
-        if x % 3 < 2 {
-          string += " ";
-        }
-      }
-      if y != 8 {
-        string += "\n";
+      string += &array[y * 9 + x].to_string();
+      if x % 3 < 2 {
+        string += " ";
       }
     }
-    string
+    if y != 8 {
+      string += "\n";
+    }
   }
+  string
 }
 
-fn solve_sudoku(sudoku: &Sudoku) -> Sudoku {
+fn solve_sudoku(sudoku: [u8; 81]) -> [u8; 81] {
   struct RecursionState {
-    solved_sudoku: Sudoku,
+    solved_sudoku: [u8; 81],
   }
+  let mut state = RecursionState { solved_sudoku: [0; 81] };
 
-  let mut state = RecursionState {
-    solved_sudoku: Sudoku::default(),
-  };
-
-  fn recur(sudoku: Sudoku, state: &mut RecursionState) -> bool {
-    // println!("\n{}", sudoku.to_string());
-
+  fn recur(sudoku: [u8; 81], state: &mut RecursionState) -> bool {
     for y in 0..9 {
       for x in 0..9 {
-        if *sudoku.get(x, y) == 0 {
+        if sudoku[y * 9 + x] == 0 {
           for digit in 1..=9 {
-            if !sudoku.can_place(x, y, digit) {
+            if !can_place(&sudoku, x, y, digit) {
               continue;
             }
 
-            let mut next_sudoku = sudoku.clone();
-            next_sudoku.change(x, y, digit);
+            let mut next_sudoku = sudoku;
+            next_sudoku[y * 9 + x] = digit;
 
             if recur(next_sudoku, state) {
               return true;
@@ -111,33 +77,39 @@ fn solve_sudoku(sudoku: &Sudoku) -> Sudoku {
       }
     }
 
+    state.solved_sudoku = sudoku;
+
     true
   }
 
-  recur(sudoku.clone(), &mut state);
+  recur(sudoku, &mut state);
 
   state.solved_sudoku
 }
 
 fn main() {
-  let file_name = "diabolical.txt";
+  let file_name = std::env::args().nth(1).expect("Please provide the file name");
 
-  let file_content = std::fs::read_to_string(file_name).unwrap_or_else(|_| panic!("'{}' not found", file_name));
+  let file_content = std::fs::read_to_string(&file_name).unwrap_or_else(|_| panic!("'{}' not found", file_name));
 
-  let sudokus: Vec<Sudoku> = file_content
+  let sudokus: Vec<[u8; 81]> = file_content
     .lines()
-    .map(|line| Sudoku::from_str(line.split_whitespace().nth(1).unwrap()))
-    .take(20000)
+    .map(|line| string_to_array(line.split_whitespace().nth(1).unwrap()))
+    .take(50000)
     .collect();
 
   let current_time = Instant::now();
 
   let _solutions = sudokus
     .par_iter()
-    .map(|sudoku| solve_sudoku(sudoku))
-    .collect::<Vec<Sudoku>>();
+    .map(|sudoku| solve_sudoku(*sudoku))
+    .collect::<Vec<[u8; 81]>>();
 
   let duration = current_time.elapsed();
+
+  // for solution in solutions {
+  //   println!("{}\n", array_to_string(solution));
+  // }
 
   println!("{:?}", duration);
 }
